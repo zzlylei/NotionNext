4334231f40535a351ce0f6c1a1495725d6bfe8b8
zzlylei <124643914+zzlylei@users.noreply.github.com>
2025-08-01 12:34:50 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -21,9 +21,9 @@ const CONFIG = {
   HEO_HERO_TITLE_1: '分享快乐',
   HEO_HERO_TITLE_2: '与颠覆认知',
   HEO_HERO_TITLE_3: 'TANGLY1024.COM',
-  HEO_HERO_TITLE_4: '跑步上线',
-  HEO_HERO_TITLE_5: '轻松定制快乐',
-  HEO_HERO_TITLE_LINK: 'https://k.zr99.top',
+  HEO_HERO_TITLE_4: '影视上线',
+  HEO_HERO_TITLE_5: '轻松定制',
+  HEO_HERO_TITLE_LINK: 'https://tv.zr99.top',
   // 英雄区遮罩文字
   HEO_HERO_COVER_TITLE: '随便逛逛',
 
@@ -120,7 +120,7 @@ const CONFIG = {
   HEO_SOCIAL_CARD_TITLE_1: '交流频道',
   HEO_SOCIAL_CARD_TITLE_2: '加入我们的社群讨论分享',
   HEO_SOCIAL_CARD_TITLE_3: '点击加入社群',
-  HEO_SOCIAL_CARD_URL: 'https://docs.tangly1024.com/article/how-to-question',
+  HEO_SOCIAL_CARD_URL: '',
 
   // 底部统计面板文案
   HEO_POST_COUNT_TITLE: '文章数:',
