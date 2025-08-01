116b891e5023bba880191ca5fa1c795505034f27
zzlylei <124643914+zzlylei@users.noreply.github.com>
2025-08-01 12:07:28 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -24,13 +24,13 @@ const BLOG = {
   BEI_AN_LINK: process.env.NEXT_PUBLIC_BEI_AN_LINK || 'https://beian.miit.gov.cn/', // 备案查询链接，如果用了萌备等备案请在这里填写
 
   // RSS订阅
-  ENABLE_RSS: process.env.NEXT_PUBLIC_ENABLE_RSS || true, // 是否开启RSS订阅功能
+  ENABLE_RSS: process.env.NEXT_PUBLIC_ENABLE_RSS || false, // 是否开启RSS订阅功能
 
   
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
 CONTACT_EMAIL: process.env.NEXT_PUBLIC_CONTACT_EMAIL || 'y284828778@gmail.com', // 邮箱地址 例如mail@tangly1024.com
 CONTACT_TELEGRAM: process.env.NEXT_PUBLIC_CONTACT_TELEGRAM || 'https://t.me/zzlylei', // 你的telegram 地址 例如 https://t.me/tangly_1024
-CONTACT_BILIBILI: process.env.NEXT_PUBLIC_CONTACT_BILIBILI || '', // B站主页
+
 
   // 其它复杂配置
   // 原配置文件过长，且并非所有人都会用到，故此将配置拆分到/conf/目录下, 按需找到对应文件并修改即可
